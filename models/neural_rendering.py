b15ca3b6cd313fa33c5de5dd7cd3999dd59e21b7
leonpaletta <27236884+Leonpa@users.noreply.github.com>
2024-03-30 11:55:20 +0100

    New UNet Structure

--- models/neural_rendering.py
+++ models/neural_rendering.py
@@ -9,6 +9,7 @@ from torchvision.transforms import transforms
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 import torchvision.models as models
+import torch.nn.functional as F
 
 
 class VGGLoss(nn.Module):
@@ -40,96 +41,104 @@ class VGGLoss(nn.Module):
         loss = self.criterion(input_features, target_features)
         return loss
 
-
-class ChannelAttention(nn.Module):
-    def __init__(self, num_channels, num_params):
-        super(ChannelAttention, self).__init__()
-        # A simple linear layer to transform the rig parameters into attention scores
-        self.attention = nn.Sequential(
-            nn.Linear(num_params, num_channels),
-            nn.Sigmoid()  # Ensure scores are in [0, 1] range
+class DoubleConv(nn.Module):
+    """(Convolution => [BN] => ReLU) * 2"""
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
         )
 
-    def forward(self, x, rig_params):
-        # Generate attention scores for each channel
-        scores = self.attention(rig_params).unsqueeze(2).unsqueeze(3)  # Reshape to match spatial dimensions
-        return x * scores  # Apply attention scores to feature map
+    def forward(self, x):
+        return self.double_conv(x)
 
 
-class AdaptiveInstanceNorm(nn.Module):
-    def __init__(self, num_features, num_params):
-        super(AdaptiveInstanceNorm, self).__init__()
-        self.num_features = num_features
-        self.linear = nn.Linear(num_params, num_features * 2)
-        self.linear.weight.data[:, :num_features].normal_(1, 0.02)  # Initialise scale at N(1, 0.02)
-        self.linear.weight.data[:, num_features:].zero_()  # Initialise bias at 0
-
-    def forward(self, x, rig_params):
-        # Obtain scale and bias
-        scale_bias = self.linear(rig_params)
-        scale, bias = scale_bias.chunk(2, 1)
-        scale = scale.unsqueeze(2).unsqueeze(3).expand_as(x)
-        bias = bias.unsqueeze(2).unsqueeze(3).expand_as(x)
-        return scale * x + bias
-
-
-class SurrogateCNN(nn.Module):
-    def __init__(self, num_params):
-        super(SurrogateCNN, self).__init__()
-        # Define the number of rigging parameters:
-
-        # Convolutional layers
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),  # Input: 3x512x512
-            nn.ReLU(),
-            nn.MaxPool2d(2),  # Output: 16x256x256
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),  # Output: 32x128x128
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),  # Output: 64x64x64
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
         )
 
-        self.fc_layers = nn.Sequential(
-            nn.Linear(64 * 64 * 64 + num_params, 1024),  # Combine with params
-            nn.ReLU(),
-        )
+    def forward(self, x):
+        return self.maxpool_conv(x)
 
-        self.adinorm1 = AdaptiveInstanceNorm(1024, num_params)
+class Up(nn.Module):
+    """Upscaling then double conv"""
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
 
-        self.attention1 = ChannelAttention(64, num_params)  # Assuming 64 channels in the feature map
+        self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
 
-        self.upsample_layers = nn.Sequential(
-            nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1),  # Output: 512x2x2
-            nn.ReLU(),
-            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),  # Output: 256x4x4
-            nn.ReLU(),
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),  # Output: 128x8x8
-            nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # Output: 64x16x16
-            nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),  # Output: 32x32x32
-            nn.ReLU(),
-            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),  # Output: 16x64x64
-            nn.ReLU(),
-            nn.ConvTranspose2d(16, 8, kernel_size=4, stride=2, padding=1),  # Output: 8x128x128
-            nn.ReLU(),
-            nn.ConvTranspose2d(8, 4, kernel_size=4, stride=2, padding=1),  # Output: 4x256x256
-            nn.ReLU(),
-            nn.ConvTranspose2d(4, 3, kernel_size=4, stride=2, padding=1),  # Output: 3x512x512
-            nn.Tanh(),
-        )
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class SurrogateUNet(nn.Module):
+    def __init__(self, n_channels, n_classes, n_rig_params, bilinear=True):
+        super(SurrogateUNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+        # Process rig parameters
+        self.fc_rig_params = nn.Linear(n_rig_params, 64 * 64 * factor)
 
     def forward(self, idle_image, rig_params):
-        features = self.conv_layers(idle_image)
-        attended_features = self.attention1(features, rig_params)
-        combined_input = torch.cat((attended_features.view(attended_features.size(0), -1), rig_params), dim=1)
-        intermediate = self.fc_layers(combined_input)
-        intermediate = intermediate.view(-1, 1024, 1, 1)
-        # normalized_features = self.adinorm1(intermediate, rig_params)
-        output = self.upsample_layers(intermediate)
-        return output
+        rig_params = self.fc_rig_params(rig_params)
+        rig_params = rig_params.view(-1, 64, 64, 1)
+        rig_params = rig_params.repeat(1, 1, 1, idle_image.shape[3] // 64) # Adjust to match spatial dimensions
+
+        x1 = self.inc(idle_image)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
 
 
 class RenderDataset(torch.utils.data.Dataset):
