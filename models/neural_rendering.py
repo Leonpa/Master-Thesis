8ddb7b2b7286361e0f073cd95938d41df67c0d5b
leonpaletta <27236884+Leonpa@users.noreply.github.com>
2024-04-03 23:52:38 +0200

    New simpler simplenet class, this time with the right conv sizes

--- models/neural_rendering.py
+++ models/neural_rendering.py
@@ -99,10 +99,11 @@ class SimpleNet(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         self.fc1 = nn.Linear(n_rig_params, 128)
-        self.fc_comb = nn.Linear(64 * 128 * 128 + 128, 2048)
+        self.fc_comb = nn.Linear(64 * 64 * 128 + 128, 2048)
 
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=4, stride=2, padding=1),
@@ -128,12 +129,14 @@ class SimpleNet(nn.Module):
         x = F.relu(self.conv2(x))
         x = self.pool(x)
         x = F.relu(self.conv3(x))
+        x = self.pool(x)
+        x = F.relu(self.conv4(x))
         x = torch.flatten(x, 1)
 
-
         rig = F.relu(self.fc1(rig_params))
-
+        # print(x.shape, rig.shape)
         x = torch.cat((x, rig), dim=1)
+        # print(x.shape)
         x = F.relu(self.fc_comb(x))
         x = x.view(-1, 128, 4, 4)  # Reshape to feed into decoder
         x = self.decoder(x)
